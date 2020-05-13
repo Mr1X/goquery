e8afbd9062676ca1b3e47d53f26c157d01dd3f1c
x <113@qq.com>
2020-05-13 16:56:38 +0800

    text with some tag

---
+++ export.go
@@ -0,0 +1,50 @@
+package goquery
+
+import (
+	"bytes"
+	"fmt"
+
+	"golang.org/x/net/html"
+)
+
+// TextWithTag query text and retain some tag like <p>,<img>
+func TextWithTag(nodes []*html.Node) string {
+	var buf bytes.Buffer
+
+	// Slightly optimized vs calling Each: no single selection object created
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type != html.TextNode {
+			switch n.Data {
+			case "p":
+				buf.WriteString("<p>")
+			case "img":
+				for _, v := range n.Attr {
+					if v.Key == "src" {
+						buf.WriteString(fmt.Sprintf(`<img src="%v">`, v.Val))
+					}
+				}
+			}
+
+		} else {
+			buf.WriteString(n.Data)
+		}
+
+		if n.FirstChild != nil {
+			for c := n.FirstChild; c != nil; c = c.NextSibling {
+				f(c)
+			}
+		}
+		if n.Type != html.TextNode {
+			switch n.Data {
+			case "p":
+				buf.WriteString("</p>")
+			}
+		}
+	}
+	for _, n := range nodes {
+		f(n)
+	}
+
+	return buf.String()
+}
--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module github.com/PuerkitoBio/goquery
 
 require (
 	github.com/andybalholm/cascadia v1.1.0
+	github.com/apex/log v1.1.4
 	golang.org/x/net v0.0.0-20200202094626-16171245cfb2
 )
 
