490fc05e15bf0fe80d4fcf97cce4ef84f514f74f
x <113@qq.com>
2020-05-14 18:17:21 +0800

    unit test

--- example_test.go
+++ example_test.go
@@ -12,8 +12,8 @@ import (
 // This example scrapes the reviews shown on the home page of metalsucks.net.
 func TestExample(t *testing.T) {
 	// Request the HTML page.
-	// res, err := http.Get("https://finance.sina.com.cn/stock/stockptd/2020-05-13/doc-iircuyvi2920672.shtml")
-	res, err := http.Get("https://finance.sina.com.cn/stock/jsy/2020-05-14/doc-iircuyvi3071692.shtml")
+	res, err := http.Get("https://finance.sina.com.cn/stock/stockptd/2020-05-13/doc-iircuyvi2920672.shtml")
+	// res, err := http.Get("https://finance.sina.com.cn/stock/jsy/2020-05-14/doc-iircuyvi3071692.shtml")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -27,10 +27,15 @@ func TestExample(t *testing.T) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// log.Println(strings.TrimPrefix(TextWithTag(doc.Find(".article").Nodes), TextWithTag(doc.Find(".ct_hqimg").Nodes)))
-	// log.Println(strings.TrimPrefix(TextWithTag(doc.Find(".article").Nodes), TextWithTag(doc.Find("blockquote").Nodes)))
-	log.Println(TextWithTag(doc.Find(".article").Nodes))
-	log.Println()
+	content := TextWithTag(doc.Find(".article").Nodes)
+	content = strings.TrimPrefix(content, TextWithTag(doc.Find(".ct_hqimg").Nodes))
+	content = strings.TrimPrefix(content, TextWithTag(doc.Find("blockquote").Nodes))
+	content = strings.ReplaceAll(content, "<p>投资B站却巨亏？又一私募爆雷！B站暴涨150%，基金却跌剩3成！募集资金投向了哪儿？是否涉嫌违规承诺？</p>", "")
+	content = strings.TrimPrefix(content, "\n")
+	log.Printf("[%v]", len("\n"))
+	log.Printf("[%v]", content[:1] == "\n")
+	log.Printf("[%v]", content[:1])
+	log.Printf("content:[%v]", content)
 }
 
 // This example shows how to use NewDocumentFromReader from a file.
