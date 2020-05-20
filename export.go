535d7c28a7c1d4e34feb11e28f889e2af02beb3c
x <113@qq.com>
2020-05-20 15:45:51 +0800

    all tag

--- export.go
+++ export.go
@@ -35,7 +35,12 @@ func TextWithTag(nodes []*html.Node) string {
 			case "img":
 				for _, v := range n.Attr {
 					if v.Key == "src" {
-						buf.WriteString(fmt.Sprintf(`<img src="%v">`, strings.TrimPrefix(v.Val, "//")))
+						// buf.WriteString(fmt.Sprintf(`<img src="%v">`, strings.TrimPrefix(v.Val, "//")))
+						img := v.Val
+						if !strings.HasPrefix(img, "http") {
+							img = "//" + img
+						}
+						buf.WriteString(fmt.Sprintf(`<img src="%v">`, img))
 					}
 				}
 			}
@@ -62,4 +67,61 @@ func TextWithTag(nodes []*html.Node) string {
 	}
 
 	return strings.TrimPrefix(buf.String(), "\n")
+}
+
+// TextWithAllTag query text and retain all tag
+func TextWithAllTag(nodes []*html.Node) string {
+	var buf bytes.Buffer
+
+	// Slightly optimized vs calling Each: no single selection object created
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		// 注释的部分
+		if n.Type == html.CommentNode {
+			return
+		}
+
+		if n.Parent.Data == "style" {
+			return
+		}
+
+		data := strings.TrimSpace(n.Data)
+
+		// log.Infof("n.Type:%v n.Data:[%v]", n.Type, data)
+
+		if n.Type != html.TextNode {
+			switch n.Data {
+			case "img":
+				for _, v := range n.Attr {
+					if v.Key == "src" {
+						buf.WriteString(fmt.Sprintf(`<img src="%v">`, v.Val))
+					}
+				}
+			default:
+				buf.WriteString("<" + n.Data + ">")
+			}
+
+		} else {
+			buf.WriteString(data)
+		}
+
+		if n.FirstChild != nil {
+			for c := n.FirstChild; c != nil; c = c.NextSibling {
+				f(c)
+			}
+		}
+		if n.Type != html.TextNode {
+			switch n.Data {
+			case "img":
+				return
+			default:
+				buf.WriteString("</" + n.Data + ">")
+			}
+		}
+	}
+	for _, n := range nodes {
+		f(n)
+	}
+
+	return buf.String()
 }
---
+++ export_test.go
@@ -0,0 +1,63 @@
+package goquery
+
+import (
+	"log"
+	"net/http"
+	"strings"
+	"testing"
+)
+
+func TestTextWithTag(t *testing.T) {
+	// Request the HTML page.
+	res, err := http.Get("https://finance.sina.com.cn/stock/stockptd/2020-05-13/doc-iircuyvi2920672.shtml")
+	// res, err := http.Get("https://finance.sina.com.cn/stock/jsy/2020-05-14/doc-iircuyvi3071692.shtml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	content := TextWithTag(doc.Find(".article").Nodes)
+	content = strings.TrimPrefix(content, TextWithTag(doc.Find(".ct_hqimg").Nodes))
+	content = strings.TrimPrefix(content, TextWithTag(doc.Find("blockquote").Nodes))
+	content = strings.ReplaceAll(content, "<p>投资B站却巨亏？又一私募爆雷！B站暴涨150%，基金却跌剩3成！募集资金投向了哪儿？是否涉嫌违规承诺？</p>", "")
+	content = strings.TrimPrefix(content, "\n")
+	log.Printf("[%v]", len("\n"))
+	log.Printf("[%v]", content[:1] == "\n")
+	log.Printf("[%v]", content[:1])
+	log.Printf("content:[%v]", content)
+}
+
+func TestTextWithAllTag(t *testing.T) {
+	res, err := http.Get("https://finance.sina.com.cn/stock/stockptd/2020-05-13/doc-iircuyvi2920672.shtml")
+	// res, err := http.Get("https://finance.sina.com.cn/stock/jsy/2020-05-14/doc-iircuyvi3071692.shtml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	content := TextWithAllTag(doc.Find(".article").Nodes)
+	content = strings.TrimPrefix(content, TextWithTag(doc.Find(".ct_hqimg").Nodes))
+	content = strings.TrimPrefix(content, TextWithTag(doc.Find("blockquote").Nodes))
+	content = strings.ReplaceAll(content, "<p>投资B站却巨亏？又一私募爆雷！B站暴涨150%，基金却跌剩3成！募集资金投向了哪儿？是否涉嫌违规承诺？</p>", "")
+	content = strings.TrimPrefix(content, "\n")
+	log.Printf("[%v]", len("\n"))
+	log.Printf("[%v]", content[:1] == "\n")
+	log.Printf("[%v]", content[:1])
+	log.Printf("content:[%v]", content)
+}
