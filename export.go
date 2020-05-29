ff24f9def6157a70a6212b87a0259e6e7de29508
x <113@qq.com>
2020-05-29 11:38:16 +0800

    TextOnlyPImg

--- export.go
+++ export.go
@@ -5,8 +5,56 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/go-xweb/log"
 	"golang.org/x/net/html"
 )
+
+// TextOnlyPImg query text and retain some tag like <p>,<img>
+func TextOnlyPImg(nodes []*html.Node) string {
+	var buf bytes.Buffer
+
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		data := ""
+		switch n.Type {
+		case html.ElementNode:
+			switch n.Data {
+			case "p":
+				buf.WriteString("<" + n.Data + ">")
+			case "img":
+				for _, v := range n.Attr {
+					if v.Key == "src" {
+						buf.WriteString(fmt.Sprintf(`<img src="%v">`, v.Val))
+					}
+				}
+			}
+		case html.TextNode:
+			data = strings.TrimSpace(n.Data)
+			buf.WriteString(data)
+		default:
+			return
+		}
+
+		log.Debugf("n.Type:%v n.Data:[%v]", n.Type, data)
+
+		if n.FirstChild != nil {
+			for c := n.FirstChild; c != nil; c = c.NextSibling {
+				f(c)
+			}
+		}
+		if n.Type == html.ElementNode {
+			switch n.Data {
+			case "p":
+				buf.WriteString("</" + n.Data + ">")
+			}
+		}
+	}
+	for _, n := range nodes {
+		f(n)
+	}
+
+	return buf.String()
+}
 
 // TextWithTag query text and retain some tag like <p>,<img>
 func TextWithTag(nodes []*html.Node) string {
@@ -20,33 +68,29 @@ func TextWithTag(nodes []*html.Node) string {
 			return
 		}
 
-		if n.Parent.Data == "style" {
+		switch n.Data {
+		case "style", "script", "iframe":
 			return
 		}
 
 		data := strings.TrimSpace(n.Data)
 
-		// log.Infof("n.Type:%v n.Data:[%v]", n.Type, data)
+		log.Debugf("n.Type:%v n.Data:[%v]", n.Type, data)
 
-		if n.Type != html.TextNode {
+		if n.Type == html.ElementNode {
 			switch n.Data {
-			case "p":
-				buf.WriteString("\n<p>")
+			case "p", "font", "ul", "li", "tr", "td":
+				buf.WriteString("<" + n.Data + ">")
 			case "img":
 				for _, v := range n.Attr {
 					if v.Key == "src" {
-						// buf.WriteString(fmt.Sprintf(`<img src="%v">`, strings.TrimPrefix(v.Val, "//")))
-						img := v.Val
-						if !strings.HasPrefix(img, "http") {
-							img = "//" + img
-						}
-						buf.WriteString(fmt.Sprintf(`<img src="%v">`, img))
+						buf.WriteString(fmt.Sprintf(`<img src="%v">`, v.Val))
 					}
 				}
 			}
 
-		} else {
-			// buf.WriteString(n.Data)
+		}
+		if n.Type == html.TextNode {
 			buf.WriteString(data)
 		}
 
@@ -55,10 +99,10 @@ func TextWithTag(nodes []*html.Node) string {
 				f(c)
 			}
 		}
-		if n.Type != html.TextNode {
+		if n.Type == html.ElementNode {
 			switch n.Data {
-			case "p":
-				buf.WriteString("</p>")
+			case "p", "font":
+				buf.WriteString("</" + n.Data + ">")
 			}
 		}
 	}
@@ -66,7 +110,7 @@ func TextWithTag(nodes []*html.Node) string {
 		f(n)
 	}
 
-	return strings.TrimPrefix(buf.String(), "\n")
+	return buf.String()
 }
 
 // TextWithAllTag query text and retain all tag
@@ -88,6 +132,69 @@ func TextWithAllTag(nodes []*html.Node) string {
 		data := strings.TrimSpace(n.Data)
 
 		// log.Infof("n.Type:%v n.Data:[%v]", n.Type, data)
+
+		if n.Type != html.TextNode {
+			switch n.Data {
+			case "img":
+				for _, v := range n.Attr {
+					if v.Key == "src" {
+						img := v.Val
+						if strings.HasPrefix(img, "//") {
+							img = strings.Replace(img, "//", "https://", 1)
+						}
+						buf.WriteString(fmt.Sprintf(`<img src="%v">`, img))
+					}
+				}
+			default:
+				buf.WriteString("<" + n.Data + ">")
+			}
+
+		} else {
+			buf.WriteString(data)
+		}
+
+		if n.FirstChild != nil {
+			for c := n.FirstChild; c != nil; c = c.NextSibling {
+				f(c)
+			}
+		}
+		if n.Type != html.TextNode {
+			switch n.Data {
+			case "img":
+				return
+			default:
+				buf.WriteString("</" + n.Data + ">")
+			}
+		}
+	}
+	for _, n := range nodes {
+		f(n)
+	}
+
+	return buf.String()
+}
+
+// TextSimple query text
+func TextSimple(nodes []*html.Node) string {
+	var buf bytes.Buffer
+
+	// Slightly optimized vs calling Each: no single selection object created
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		// 注释的部分
+		if n.Type == html.CommentNode {
+			return
+		}
+
+		switch n.Parent.Data {
+		case "style", "script":
+			return
+		}
+
+		data := strings.TrimSpace(n.Data)
+
+		log.Infof("n:%v", n)
+		log.Infof("n.Type:%v n.DataAtom:%v n.Data:[%v] n.Namespace:%v", n.Type, n.DataAtom, data, n.Namespace)
 
 		if n.Type != html.TextNode {
 			switch n.Data {
--- export_test.go
+++ export_test.go
@@ -7,6 +7,27 @@ import (
 	"testing"
 )
 
+func TestTextOnlyPImg(t *testing.T) {
+	link := "https://finance.sina.com.cn/stock/stockptd/2020-05-13/doc-iircuyvi2920672.shtml"
+	rsp, err := http.Get(link)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rsp.Body.Close()
+	if rsp.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", rsp.StatusCode, rsp.Status)
+	}
+
+	// Load the HTML document
+	doc, err := NewDocumentFromReader(rsp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	content := TextOnlyPImg(doc.Find(".article").Nodes)
+
+	log.Printf("content:[%v]", content)
+}
+
 func TestTextWithTag(t *testing.T) {
 	// Request the HTML page.
 	res, err := http.Get("https://finance.sina.com.cn/stock/stockptd/2020-05-13/doc-iircuyvi2920672.shtml")
@@ -25,13 +46,10 @@ func TestTextWithTag(t *testing.T) {
 		log.Fatal(err)
 	}
 	content := TextWithTag(doc.Find(".article").Nodes)
-	content = strings.TrimPrefix(content, TextWithTag(doc.Find(".ct_hqimg").Nodes))
-	content = strings.TrimPrefix(content, TextWithTag(doc.Find("blockquote").Nodes))
-	content = strings.ReplaceAll(content, "<p>投资B站却巨亏？又一私募爆雷！B站暴涨150%，基金却跌剩3成！募集资金投向了哪儿？是否涉嫌违规承诺？</p>", "")
-	content = strings.TrimPrefix(content, "\n")
-	log.Printf("[%v]", len("\n"))
-	log.Printf("[%v]", content[:1] == "\n")
-	log.Printf("[%v]", content[:1])
+	// content = strings.TrimPrefix(content, TextWithTag(doc.Find(".ct_hqimg").Nodes))
+	// content = strings.TrimPrefix(content, TextWithTag(doc.Find("blockquote").Nodes))
+	// content = strings.ReplaceAll(content, "<p>投资B站却巨亏？又一私募爆雷！B站暴涨150%，基金却跌剩3成！募集资金投向了哪儿？是否涉嫌违规承诺？</p>", "")
+
 	log.Printf("content:[%v]", content)
 }
 
@@ -59,5 +77,25 @@ func TestTextWithAllTag(t *testing.T) {
 	log.Printf("[%v]", len("\n"))
 	log.Printf("[%v]", content[:1] == "\n")
 	log.Printf("[%v]", content[:1])
+	log.Printf("content:[%v]", content)
+}
+
+func TestTextSimple(t *testing.T) {
+	link := "http://finance.china.com.cn/stock/usstock/20200319/5226061.shtml"
+	res, err := http.Get(link)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	content := TextWithTag(doc.Find("p").Nodes)
 	log.Printf("content:[%v]", content)
 }
